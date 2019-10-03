31bdcb9858586dba6d91ec877d72e2d0843b1b03
Shriniwas Sharma <shriniwas26@gmail.com>
2019-10-03 22:44:06 +0200

    Better update_offset script

--- update_offset.py
+++ update_offset.py
@@ -5,11 +5,14 @@ import logging
 import logging.handlers
 import os
 import json
+import sys
+import random
+import pprint
 
 # First part of the actual command:
 # `cometblue -f json device -p 1762 80:30:DC:E9:4E:50 get temperatures`
 BASE_COMMAND = "cometblue -f json device -p 1762 80:30:DC:E9:4E:50"
-SLEEP_MINUTES = 15
+SLEEP_MINUTES = 10
 
 
 def get_logger():
@@ -26,77 +29,81 @@ def get_logger():
     dirname, _filename = os.path.split(os.path.abspath(__file__))
     fh_info = logging.handlers.TimedRotatingFileHandler(
         dirname + "/logs/update_offset.log", when='midnight', backupCount=50)
-    fh_info.setLevel(logging.INFO)
+    fh_info.setLevel(logging.DEBUG)
     fh_info.setFormatter(formatter)
     logger.addHandler(fh_info)
 
     # Setup console logger
     ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
+    ch.setLevel(logging.ERROR)
     ch.setFormatter(formatter)
     logger.addHandler(ch)
     return logger
 
 
+def run_command(command):
+    logger = logging.getLogger("root")
+    logger.debug("Running command: '{}'".format(command))
+    result = subprocess.run([command], stdout=subprocess.PIPE, shell=True)
+    if result.returncode != 0:
+        logger.error("Command '{}' failed!".format(command))
+        raise RuntimeError("Command '{}' failed!".format(command))
+    return result.stdout
+
+
 def main():
     logger = get_logger()
-    error_count = 0
+    random.seed(0)
     while True:
-        if error_count > 5:
-            logger.warn(
-                "Error threshold, sleeping for {} minutes".format(SLEEP_MINUTES))
-            time.sleep(SLEEP_MINUTES * 60)
-        try:
-            logger.info("Reading DHT22 sensor...")
-            with open("/tmp/temperature_dht22.txt") as f:
-                line = f.readlines()[0]
-                dht22_temp = float(line)
-            logger.info("DHT22 Sensor reports: {} °C".format(dht22_temp))
-            logger.info("Getting battery information from cometblue...")
-            result = subprocess.run(
-                [BASE_COMMAND + ' get battery'], stdout=subprocess.PIPE, shell=True)
-            assert result.returncode == 0
-            battery_level = result.stdout.decode("utf-8")
-            logger.info("Battery: {}".format(battery_level))
+        """ Step (1) """
+        sensor_data_file = "/tmp/temperature_dht22.txt"
+        with open(sensor_data_file) as f:
+            logger.info("Read DHT22 sensor file '{}'".format(sensor_data_file))
+            line = f.readlines()[0]
+            dht22_temp = float(line)
+        logger.info(
+            "Room temperature sensor reports: {:.2f} C".format(dht22_temp))
 
-            logger.info("Reading from cometblue...")
-            result = subprocess.run(
-                [BASE_COMMAND + ' get temperatures'], stdout=subprocess.PIPE, shell=True)
-            cometblue_temperatures = json.loads(result.stdout)
-            logger.info("All temperatures: {}".format(cometblue_temperatures))
-            # Extract current temp from result
+        """ Step (2) """
+        random_num = random.random()
+        logger.debug("Random number = {:.3f}".format(random_num))
+        if random_num < 0.10:
+            logger.info("Getting battery information from thermostat...")
+            result_stdout = run_command(BASE_COMMAND + " get battery")
+            battery_level = result_stdout.decode("utf-8")
+            logger.info("Battery: {} %".format(battery_level))
 
-            cometblue_temp = float(cometblue_temperatures["current_temp"])
-            logger.info("Cometblue reports: {} °C".format(cometblue_temp))
+        if random_num < 0.05:
+            logger.info("Setting time on cometblue...")
+            _result_stdout = run_command(BASE_COMMAND + " set datetime")
+            logger.info("Time set successfully")
 
-            # Calculate correct offset
-            correct_offset = dht22_temp - cometblue_temp
-            # Round to nearest 0.5
-            correct_offset = round(correct_offset * 2) / 2
-            logger.info("Correct offset is: {} °C".format(correct_offset))
+        """ Step (3) """
+        logger.info("Reading temperature from cometblue...")
+        result_stdout = run_command(BASE_COMMAND + " get temperatures")
+        cometblue_temperatures = json.loads(result_stdout)
+        logger.info("All temperatures: \n{}".format(
+            pprint.pformat(cometblue_temperatures)))
+        cometblue_temp = float(cometblue_temperatures["current_temp"])
+        logger.info("Cometblue reports: {} C".format(cometblue_temp))
 
-            if cometblue_temperatures["offset_temp"] != correct_offset:
-                logger.info("Setting correct offset...")
-                result = subprocess.run(
-                    [BASE_COMMAND +
-                        ' set temperatures --temp-offset {}'.format(correct_offset)],
-                    stdout=subprocess.PIPE, shell=True)
-                assert result.returncode == 0
-                logger.info("Successfully set correct offset")
-            else:
-                logger.info("Offset already correct")
+        """ Step (4) """
+        correct_offset_raw = dht22_temp - cometblue_temp
+        # Round to nearest 0.5
+        correct_offset = round(correct_offset_raw * 2) / 2
+        logger.info("Correct offset is: {} C".format(correct_offset))
 
-            # Sleep
-            logger.info("Sleeping for {} minutes...".format(SLEEP_MINUTES))
-            time.sleep(SLEEP_MINUTES * 60)
+        if cometblue_temperatures["offset_temp"] != correct_offset:
+            logger.info("Setting correct offset...")
+            _result_stdout = run_command(
+                BASE_COMMAND + " set temperatures --temp-offset {}".format(correct_offset))
+            logger.info("Successfully set correct offset")
+        else:
+            logger.info("Offset is already correct")
 
-            # Reset error count
-            error_count = 0
-
-        except Exception as e:
-            error_count += 1
-            logger.error("Exception: {}".format(str(e)))
-            logger.error("Retrying...")
+        # Sleep
+        logger.info("Sleeping for {} minutes...".format(SLEEP_MINUTES))
+        time.sleep(SLEEP_MINUTES * 60)
 
 
 if __name__ == "__main__":
