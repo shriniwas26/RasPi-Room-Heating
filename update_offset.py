84e5c8ca964797f29884c2f896cfc115bac9c49f
Shriniwas <shriniwas.sharma@sioux.eu>
2019-10-04 11:43:14 +0200

    WIP: Improvements

--- update_offset.py
+++ update_offset.py
@@ -8,6 +8,7 @@ import json
 import sys
 import random
 import pprint
+import datetime as dt
 
 # First part of the actual command:
 # `cometblue -f json device -p 1762 80:30:DC:E9:4E:50 get temperatures`
@@ -51,10 +52,52 @@ def run_command(command):
     return result.stdout
 
 
-def main():
+def get_current_timeslot(active_timeslots):
+    dt_now = dt.datetime.now()
+    for timeslot in active_timeslots[dt_now.day]:
+        if timeslot["start"] <= dt_now.time() <= timeslot["end"]:
+            return timeslot
+    return None
+
+
+def main(parameter_list):
+    logger = logging.getLogger("root")
+    """ Get active hours for today """
+    while True:
+        logger.info("Reading active timeslots from cometblue...")
+        result_stdout = run_command(BASE_COMMAND + " get days")
+        active_timeslots = json.loads(result_stdout)[0]
+        # Filter out invalid timeslots: null and empty ones
+        active_timeslots = filter(lambda t: (t["start"] != t["end"] != None), active_timeslots)
+        def convert_timeslot(timeslot):
+            formatted_timeslot = {}
+            for key, value in timeslot.items():
+                only_time = dt.datetime.strptime(value, "%H:%M:%S").time()
+                date_time = dt.datetime.combine(dt.date.today(), only_time)
+                formatted_timeslot[key] = date_time
+            return formatted_timeslot
+        active_timeslots = map(convert_timeslot, active_timeslots)
+        current_timeslot = get_current_timeslot(active_timeslots)
+        if current_timeslot is None:
+            # Find immediate next timeslot, and sleep for the difference
+            time_to_sleep = dt.timedelta(days=10)  # 10 days is obviously greater than everything
+            for timeslot in active_timeslots:
+                delta = timeslot["start"] - dt.datetime.now()
+                if delta.total_seconds() > 0:
+                    time_to_sleep = min(time_to_sleep, delta)
+            logger.info("Currently not active, Sleeping for {} ...".format(time_to_sleep))
+            time.sleep(time_to_sleep.total_seconds())
+        else:
+            current_timeslot_str = "{} - {}".format(current_timeslot["start"], current_timeslot["end"])
+            logger.info("Current active timeslot is: {}".format(current_timeslot_str))
+            heating_loop(current_timeslot["end"])
+            logger.info("Timeslot ended: {}".format(current_timeslot_str))
+
+
+def heating_loop(end_time):
     logger = get_logger()
     random.seed(0)
-    while True:
+    while dt.datetime.now() < end_time:
         """ Step (1) """
         sensor_data_file = "/tmp/temperature_dht22.txt"
         with open(sensor_data_file) as f:
