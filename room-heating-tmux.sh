30507a44ffa0618f961fd9370b51e3cfd4552cc4
Shriniwas <shriniwas26@gmail.com>
2020-05-22 03:21:21 +0200

    SysAdmin stuff added
    * Systemd service files added
    * tmux launch script

---
+++ room-heating-tmux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SESSIONNAME="workspace"
+PROJECT_FOLDER="/home/pi/RasPi-Room-Heating"
+
+tmux has-session -t $SESSIONNAME &> /dev/null
+if [ $? != 0 ]
+
+    then
+        # new session with name $SESSIONNAME and window 0 named "base"
+        tmux new-session -s $SESSIONNAME -n "Logs" -d
+        tmux send-keys "tail -n 100 -f ${PROJECT_FOLDER}/logs/update_offset.log" 'C-m'
+
+        tmux splitw -v -p 50 # split it into two halves
+
+        tmux send-keys "tail -n 100 -f ${PROJECT_FOLDER}/logs/room_weather.log" 'C-m'
+
+        tmux splitw -h -p 50 # split it into two halves
+
+fi
+
+tmux attach -t $SESSIONNAME
