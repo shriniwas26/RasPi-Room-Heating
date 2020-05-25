81f0026e614307faacef4c0ec5cde7c926ce261e
Shriniwas <shriniwas26@gmail.com>
2020-05-25 13:29:21 +0200

    Update to tmux launch script

--- room-heating-tmux.sh
+++ room-heating-tmux.sh
@@ -3,11 +3,13 @@
 SESSIONNAME="workspace"
 PROJECT_FOLDER="/home/pi/RasPi-Room-Heating"
 
-if [ "tmux has-session -t $SESSIONNAME &> /dev/null" != 0 ]
+tmux has-session -t $SESSIONNAME &> /dev/null
+HAS_SESSION=$?
 
+if [[ ${HAS_SESSION} != 0 ]]
     then
         # new session with name $SESSIONNAME and window 0 named "base"
-        tmux new-session -s $SESSIONNAME -n "Logs" -d
+        tmux new-session -s ${SESSIONNAME} -n "Logs" -d
         tmux send-keys "tail -n 100 -f ${PROJECT_FOLDER}/logs/update_offset.log" 'C-m'
 
         tmux splitw -v -p 50 # split it into two halves
@@ -15,7 +17,6 @@ if [ "tmux has-session -t $SESSIONNAME &> /dev/null" != 0 ]
         tmux send-keys "tail -n 100 -f ${PROJECT_FOLDER}/logs/room_weather.log" 'C-m'
 
         tmux splitw -h -p 50 # split it into two halves
-
 fi
 
-tmux attach -t $SESSIONNAME
+tmux attach -t ${SESSIONNAME}
